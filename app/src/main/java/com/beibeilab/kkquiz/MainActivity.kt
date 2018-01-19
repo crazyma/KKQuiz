141b142343cea45a027d474feb4c65718ddcb593
David-Retina15 <david77115@gmail.com>
2018-01-19 15:08:32 +0800

    refactor: clean codde in MainActivity

--- app/src/main/java/com/beibeilab/kkquiz/MainActivity.kt
+++ app/src/main/java/com/beibeilab/kkquiz/MainActivity.kt
@@ -10,17 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val songList = ArrayList<String>()
-        songList.add("OseG-8qU8UtszwJlXm")
-        songList.add("4ql_l_98WUFosMGFiW")
-
         FragmentUtils.setupFragment(
                 this,
                 SearchFragment.newInstance(),
                 R.id.fragment_content
         )
-
-        val apiKey = BuildConfig.KKBOX_ACCESS_TOKEN
 
     }
 
